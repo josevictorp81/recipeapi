7228ea8e3f31ac1411999289d1881eedb7d4acae
Jose Victor Pereira Lima <josevictorpy@gmail.com>
2022-08-16 20:06:22 -0300

    app core, model user + tests criar usuário

---
+++ core/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+
+class User(PermissionsMixin, AbstractBaseUser):
+    email = models.EmailField(max_length=100, unique=True)
+    name = models.CharField(max_length=150)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+
+    def __str__(self) -> str:
+        return self.email
